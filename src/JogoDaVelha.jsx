7c6a82e2a4038c98cc1a59f3bc8dae2c926965c9
Prof. Mário Torres <mario.torres@oceanbrasil.com>
2026-06-20 11:19:58 -0400

    Tabuleiro

--- src/JogoDaVelha.jsx
+++ src/JogoDaVelha.jsx
@@ -1,5 +1,32 @@
+import './Jogo.css'
+
+function Tabuleiro() {
+    return (
+        <>
+        <div>
+            <button className='q'></button>
+            <button className='q'></button>
+            <button className='q'></button>
+        </div>
+        <div>
+            <button className='q'></button>
+            <button className='q'></button>
+            <button className='q'></button>
+        </div>
+        <div>
+            <button className='q'></button>
+            <button className='q'></button>
+            <button className='q'></button>
+        </div>
+        </>
+    )
+}
+
 export default function JogoDaVelha() {
     return (
-        <h1>Jogo da velha</h1>
+        <>
+            <h1>Jogo da velha</h1>
+            <Tabuleiro/>
+        </>
     )
 }
