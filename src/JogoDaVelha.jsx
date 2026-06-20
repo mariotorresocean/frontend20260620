b0151992474a2dd01d1bd336df19ffd6a817b0dd
Prof. Mário Torres <mario.torres@oceanbrasil.com>
2026-06-20 11:42:39 -0400

    Vez do X

--- src/JogoDaVelha.jsx
+++ src/JogoDaVelha.jsx
@@ -1,22 +1,38 @@
+import { useState } from 'react'
 import './Jogo.css'
 
+function Quadrado() {
+    const [quad, setQuad] = useState('')
+    return (
+        <button className='q' onClick={(quad)=>setQuad('X')}>{quad}</button>
+    )
+}
+
 function Tabuleiro() {
+    const [vezDoX,setVezDoX] = useState(true)
+    let mensagem = 'Vez do ';
+    if (vezDoX) {
+        mensagem = mensagem + 'X'; 
+    } else {
+        mensagem = mensagem + 'O';
+    }
     return (
         <>
+        <h3>{mensagem} </h3>
         <div>
-            <button className='q'></button>
-            <button className='q'></button>
-            <button className='q'></button>
+            <Quadrado />
+            <Quadrado />
+            <Quadrado />
         </div>
         <div>
-            <button className='q'></button>
-            <button className='q'></button>
-            <button className='q'></button>
+            <Quadrado />
+            <Quadrado />
+            <Quadrado />
         </div>
         <div>
-            <button className='q'></button>
-            <button className='q'></button>
-            <button className='q'></button>
+            <Quadrado />
+            <Quadrado />
+            <Quadrado />
         </div>
         </>
     )
